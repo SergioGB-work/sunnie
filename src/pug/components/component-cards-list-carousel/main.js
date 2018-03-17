34861493b25bcc030640a8851ace4ed04f611ec1
Sergio González Barrios <sgonzalez@aventia.com>
2018-03-17 21:26:29 +0100

    Añadido componente slider y creados diversos fragmmentos

--- src/pug/components/component-cards-list-carousel/main.js
+++ src/pug/components/component-cards-list-carousel/main.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
         slideSpeed : 300,
         paginationSpeed : 400,
         pagination: false,
-        items : 4,
-        itemsDesktop : 4,
-        itemsDesktopSmall : 3, 
-        itemsTablet: 3,
-        itemsMobile :  2
+        items : 4, //10 items above 1000px browser width
+        itemsDesktop : [1000,4], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
      });
     
 })
---
+++ src/pug/components/component-slider/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        
+    $(".component-slider .component-content").owlCarousel({
+
+        navigationText:['<span class="fa fa-angle-left" aria-hidden="true"></span>','<span class="fa fa-angle-right" aria-hidden="true"></span>'],
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+     });
+    
+})
