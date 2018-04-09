a2ccaf777138bb3125084739ccba67a222155623
SergioGB-work <chekis87@hotmail.com>
2018-04-09 09:29:33 +0200

    Ajustes mobile

--- src/pug/components/component-cards-list-carousel/main.js
+++ src/pug/components/component-cards-list-carousel/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         itemsDesktop : [1000,4], //5 items between 1000px and 901px
         itemsDesktopSmall : [900,3], // betweem 900px and 601px
         itemsTablet: [600,2], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        itemsMobile : [480,1]
      });
     
 })
--- src/pug/components/component-slider/main.js
+++ src/pug/components/component-slider/main.js
@@ -6,7 +6,27 @@ $(document).ready(function(){
         navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
+        singleItem:true,
+        afterAction: function(owl){
+
+            if($(window).width() < 768){
+                $(owl).find('.fragment img').each(function(){
+                    if($(this).data('responsive-src') && $(this).data('responsive-src') != $(this).attr('src')){
+                        $(this).attr('src',$(this).data('responsive-src'));
+                    }
+                });
+            }
+
+            else{
+            	$(owl).find('.fragment img').each(function(){
+
+ 					if($(this).data('src') && $(this).data('src') != $(this).attr('src')){
+                        $(this).attr('src',$(this).data('src'));
+                    }            		
+
+            	});
+            }
+        }        
      });
     
 })
